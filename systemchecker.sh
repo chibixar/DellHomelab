49d84930c1e4cc5148fa985acb45d7fe073836f5
chibixar <minkevicharkadz@gmail.com>
2025-12-02 22:21:14 +0300

    Echo Monitoring

--- systemchecker.sh
+++ systemchecker.sh
@@ -2,6 +2,14 @@
 
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')   # b - batch mode (print to terminal, not interactive); n1 - show 1 iteration and exit (no refreshing); $2 - for user cpu; $4 - for system cpu
 
+MEMORY=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+
+DISK=$(df -h / | awk 'NR==2 {print $5}') # h - for human-readable form 
+
+UPTIME=$(uptime -p) # p - for human-readable form
+
+TOP_PROCESSES=$(ps -eo pid,cmd,%cpu --sort=-%cpu | head -n 6) # e = aux; o - specifies output format by columns
+
 echo "System Monitoring Report - $(date)"
 echo "-----------------------------------------"
 echo "CPU Usage: $CPU%"
